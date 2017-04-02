62a8855217cd4ec4e16add4f5b5ef41a8a909618
Maksym <4ygakk@gmail.com>
2017-04-02 20:59:35 +0200

    first

---
+++ EDwL4/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace EDwL4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 98);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(278, 201);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(222, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 311);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ EDwL4/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDwL4
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string urlAddress = textBox1.Text;
+
+            Uri myUri = new Uri(urlAddress);
+            var ip = Dns.GetHostAddresses(myUri.Host)[0];
+            string[] row = { urlAddress + "-"+ ip};
+            richTextBox1.AppendText(urlAddress + "-" + ip);
+            richTextBox1.AppendText(System.Environment.NewLine);
+
+            int nrounds = Convert.ToInt32(textBox2.Text);
+
+
+            GetAllLinks(urlAddress,Convert.ToString(ip),nrounds);
+
+
+        }
+        public void GetAllLinks(string urlAddress, string ip, int nrounds)
+        {
+            
+            if (count < nrounds)
+            {
+                count++;
+                string[] strok = File.ReadAllLines("out.txt");
+                if (strok.Length == 0)
+                {
+
+                }
+                else
+                {
+                    urlAddress = Regex.Replace(strok[i], @"\s([^>]*)", "");
+                    Uri myUri = new Uri(urlAddress);
+                    var newip = Dns.GetHostAddresses(myUri.Host)[0];
+                    ip = Convert.ToString(newip);
+                    i += 2;
+                }
+
+                Match m;
+                string HRefPattern = @"\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]";
+                //@"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = null;
+
+                    if (response.CharacterSet == null)
+                    {
+                        readStream = new StreamReader(receiveStream);
+                    }
+                    else
+                    {
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding("Windows-1250"));
+                    }
+
+                    string data = readStream.ReadToEnd();
+                    data = Regex.Replace(data, @"<meta([^>]*)>", "");
+                    //data = Regex.Replace(data, @"<style>([^>]*)<\/style>", "");
+                    //data = Regex.Replace(data, @"<script>([^>]*)<\/script>", "");
+
+
+
+                    try
+                    {
+                        m = Regex.Match(data, HRefPattern,
+                                        RegexOptions.IgnoreCase | RegexOptions.Compiled,
+                                        TimeSpan.FromSeconds(1));
+                        while (m.Success)
+                        {
+                            Uri myUri = new Uri(m.Value);
+                            var ip2 = Dns.GetHostAddresses(myUri.Host)[0];
+                            richTextBox1.AppendText(m.Value + " " + ip2);
+                            richTextBox1.AppendText(System.Environment.NewLine);
+                            m = m.NextMatch();
+
+                            if (ip == Convert.ToString(ip2))
+                            {
+                                string appendText = m.Value + " " + ip2 + '\n';
+                                File.AppendAllText("in.txt", appendText, Encoding.UTF8);
+                                File.AppendAllText("in.txt", System.Environment.NewLine, Encoding.UTF8);
+                            }
+                            else
+                            {
+                                string appendText = m.Value + " " + ip2 + '\n';
+                                File.AppendAllText("out.txt", appendText, Encoding.UTF8);
+                                File.AppendAllText("out.txt", System.Environment.NewLine, Encoding.UTF8);
+                            }
+                        }
+                    }
+                    catch (RegexMatchTimeoutException)
+                    {
+                        Console.WriteLine("The matching operation timed out.");
+                    }
+                     GetAllLinks(urlAddress, ip, nrounds);
+                }
+            }
+            return;
+            
+            
+        }
+
+    }
+}
